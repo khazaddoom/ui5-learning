a40985f2bf33df7e9717d75741795b59c76a5594
khazaddoom <31121986g@gmail.com>
2026-02-18 07:45:36 +0530

    day-2

--- webapp/index.ts
+++ webapp/index.ts
@@ -1,1 +1,19 @@
-alert("UI5 is ready");
+// import Text from 'sap/m/Text';
+// import Avatar from 'sap/m/Avatar';
+
+// new Text({
+//     text: 'Welcome to my new Learning Journey with SAPUI5'
+// }).placeAt('content');
+
+// new Avatar({
+//     src: "https://gravatar.com/avatar/8a37f049e6876cc91b7c827fc9fbe05f?s=400&d=robohash&r=x"
+// }).placeAt('profile');
+
+import XMLView from "sap/ui/core/mvc/XMLView";
+
+
+XMLView.create({
+    viewName: "ui5.walkthrough.view.App"
+}).then(function (oView) {
+    oView.placeAt("content");
+});
