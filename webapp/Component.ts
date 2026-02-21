0146eca3445a2c8e5c035f560b79968bcec2b114
khazaddoom <31121986g@gmail.com>
2026-02-22 01:08:16 +0530

    day5

--- webapp/Component.ts
+++ webapp/Component.ts
@@ -2,6 +2,8 @@ import Control from "sap/ui/core/Control";
 import XMLView from "sap/ui/core/mvc/XMLView";
 import UIComponent from "sap/ui/core/UIComponent";
 import JSONModel from "sap/ui/model/json/JSONModel";
+import ResourceModel from "sap/ui/model/resource/ResourceModel";
+import ResourceBundle from "sap/base/i18n/ResourceBundle";
 
 /**
  * @namespace ui5.walkthrough
@@ -19,6 +21,12 @@ export default class Component extends UIComponent {
         }
         const oModel = new JSONModel(data)
         this.setModel(oModel);
+
+        const i18nModel = new ResourceModel({
+            bundleName: "ui5.walkthrough.i18n.i18n"
+        })
+        this.setModel(i18nModel, "i18n")
+
     };
     createContent(): Control | Promise<Control | null> | null {
         return XMLView.create({
