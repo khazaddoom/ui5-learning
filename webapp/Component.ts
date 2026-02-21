5a8b1ca589995520987d3734db0544b2a16f1a72
khazaddoom <31121986g@gmail.com>
2026-02-22 01:15:48 +0530

    day5
    moved configurations, removed index.ts file

--- webapp/Component.ts
+++ webapp/Component.ts
@@ -2,15 +2,14 @@ import Control from "sap/ui/core/Control";
 import XMLView from "sap/ui/core/mvc/XMLView";
 import UIComponent from "sap/ui/core/UIComponent";
 import JSONModel from "sap/ui/model/json/JSONModel";
-import ResourceModel from "sap/ui/model/resource/ResourceModel";
-import ResourceBundle from "sap/base/i18n/ResourceBundle";
 
 /**
  * @namespace ui5.walkthrough
  */
 export default class Component extends UIComponent {
     public static metadata = {
-        "interfaces": ["sap.ui.core.IAsyncContentCreation"]
+        "interfaces": ["sap.ui.core.IAsyncContentCreation"],
+        "manifest": "json"
     };
     init(): void {
         super.init();
@@ -22,16 +21,5 @@ export default class Component extends UIComponent {
         const oModel = new JSONModel(data)
         this.setModel(oModel);
 
-        const i18nModel = new ResourceModel({
-            bundleName: "ui5.walkthrough.i18n.i18n"
-        })
-        this.setModel(i18nModel, "i18n")
-
     };
-    createContent(): Control | Promise<Control | null> | null {
-        return XMLView.create({
-            "viewName": "ui5.walkthrough.view.App",
-            "id": "app"
-        })
-    }
 }
