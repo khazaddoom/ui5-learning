0b4e931e1f30442afe3f282456f4d6893a49c227
khazaddoom <31121986g@gmail.com>
2026-02-27 08:08:42 +0530

    day-6
    added fragment

--- webapp/controller/HelloPanel.controller.ts
+++ webapp/controller/HelloPanel.controller.ts
@@ -1,7 +1,21 @@
+import Dialog from "sap/m/Dialog";
 import Controller from "sap/ui/core/mvc/Controller";
 
 /**
  * @namespace ui5.walkthrough.controller
  */
 export default class HelloPanelController extends Controller {
+
+    private dialog: Dialog;
+
+    async openDialog(): Promise<void> {
+        this.dialog ??= await this.loadFragment({
+            name: "ui5.walkthrough.view.HelloDialog"
+        }) as Dialog;
+        this.dialog.open();
+    }
+
+    async onCloseDialog(): Promise<void> {
+        (this.byId("helloDialog") as Dialog)?.close()
+    }
 }
